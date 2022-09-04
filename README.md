0526006d249ce95afca5ef4d1198cf854fd69ff4
admiswalker <admiswalker@gmail.com>
2022-09-04 23:32:04 +0900

    modify run_test.sh

--- README.md
+++ README.md
@@ -7,3 +7,12 @@
 ./docker/build.sh
 ```
 
+## 使い方
+### テストの実行
+```bash
+./run_test.sh
+```
+### 特定のテストだけ実行
+```bash
+./run_test.sh test/test_xxx.py
+```
--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+TARGET=./test/runner.py
+if [ $# != 1 ]; then
+    echo 'RUN: ./test/runner.py'
+else
+    TARGET=$1
+    echo 'RUN: '${TARGET}
+fi
+
 docker run \
        -u `id -u`:`id -g` \
        --rm -it --name docker_py_dev_env_template \
        -v $PWD:/home -w /home \
-       docker_py_dev_env_template:latest python3.10 ./test/runner.py
+       docker_py_dev_env_template:latest python3.10 ${TARGET}
